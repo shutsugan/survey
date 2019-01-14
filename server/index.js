efd0e60ece107f5affd6b7b071e0c8e3d4b4033e
shutsugan <muhammad@nextmedia.ma>
2019-01-14 23:51:17 +0100

    chore (server): change the value of the hi object

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const PORT = process.env.PORT || 4000;
 const app = express();
 
 app.get('/', (req, res) => {
-	res.send({hi: 'Hi'});
+	res.send({hi: 'Hollow...'});
 });
 
 app.listen(PORT, _ => console.log('Running on 5000...'));
