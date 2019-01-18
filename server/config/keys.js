abe08bdf1060b805f22d0481cc61b18a50acf33a
shutsugan <muhammad@nextmedia.ma>
2019-01-18 21:58:06 +0100

    chore (mongodb): set database connection
    import mongoose and
    configure it in server

--- server/config/keys.js
+++ server/config/keys.js
@@ -1,4 +1,5 @@
 module.exports = {
 	googleClientID: '1030755326582-ngf2encldde8toaot0tb1v7a3ieedt0r.apps.googleusercontent.com',
-	googleClientSecret: 'OKfGOSQ7SF0YVcK9TWdFmwTb'
+	googleClientSecret: 'OKfGOSQ7SF0YVcK9TWdFmwTb',
+	mongoDbURI: 'mongodb://shutsugan:root10@ds159624.mlab.com:59624/servy'
 }
--- server/index.js
+++ server/index.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
+const keys = require('./config/keys');
 require('./services/passport');
 
+mongoose.connect(keys.mongoDbURI);
 
 const PORT = process.env.PORT || 5000;
 const app = express();
