642e3d3fc14bc3744d8930fab2370923e2da0c7b
shutsugan <muhammad@nextmedia.ma>
2019-01-23 00:09:49 +0100

    feat (stripe): set stripe call to the server placeholder

--- server/client/src/actions/index.js
+++ server/client/src/actions/index.js
@@ -6,3 +6,8 @@ export const fetchUser = _ => async dispatch => {
 	const {data} = await axios.get('/api/current_user');
 	dispatch({type: FETCH_USER, payload: data});
 };
+
+export const handleToken = token => async dispatch => {
+	const {data} = await axios.post('/api/stripe', token);
+	dispatch({type: FETCH_USER, payload: data});
+};
--- server/client/src/components/Payment/index.js
+++ server/client/src/components/Payment/index.js
@@ -1,19 +1,20 @@
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
+import { connect } from 'react-redux';
+import * as actions from '../../actions';
 
-const Payment = _ => {
+const Payment = ({handleToken}) => {
   return (
     <StripeCheckout
       name="Servy"
       description="$5 for 5 email credits"
       amount={500}
-      token={token => console.log(token)}
+      token={token => handleToken(token)}
       stripeKey={process.env.REACT_APP_STRIPE_KEY}>
 
       <button className="button button--pay">Add credits</button>
     </StripeCheckout>
-
   );
 };
 
-export default Payment;
+export default connect(null, actions)(Payment);
